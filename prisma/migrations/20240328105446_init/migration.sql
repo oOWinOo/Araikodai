21dbc1281ed952d35a390ec64ab78fbb960be7c8
bruce <bruceoio@hotmail.com>
2024-03-28 17:55:00 +0700

    feat: delete unused

---
+++ prisma/migrations/20240328105446_init/migration.sql
@@ -0,0 +1,74 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "birthDate" DATETIME NOT NULL,
+    "telephoneNumber" TEXT NOT NULL,
+    "money" INTEGER NOT NULL,
+    "profileImage" TEXT
+);
+
+-- CreateTable
+CREATE TABLE "Admin" (
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+    "published" BOOLEAN DEFAULT false,
+    "authorId" INTEGER,
+    CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Discount" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "type" TEXT NOT NULL,
+    "value" TEXT NOT NULL,
+    "remaining" INTEGER NOT NULL DEFAULT -1
+);
+
+-- CreateTable
+CREATE TABLE "Hotel" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "imageURL" TEXT NOT NULL,
+    "telephone" TEXT NOT NULL,
+    "detail" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Room" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "imageURL" TEXT NOT NULL,
+    "price" REAL NOT NULL,
+    "occupancy" INTEGER NOT NULL,
+    "hotelId" INTEGER NOT NULL,
+    CONSTRAINT "Room_hotelId_fkey" FOREIGN KEY ("hotelId") REFERENCES "Hotel" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Booking" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "hotelId" INTEGER NOT NULL,
+    "roomId" INTEGER,
+    "startdate" DATETIME NOT NULL,
+    "bookingDays" INTEGER NOT NULL,
+    CONSTRAINT "Booking_hotelId_fkey" FOREIGN KEY ("hotelId") REFERENCES "Hotel" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Booking_roomId_fkey" FOREIGN KEY ("roomId") REFERENCES "Room" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Admin_username_key" ON "Admin"("username");

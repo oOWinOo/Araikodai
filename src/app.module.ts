53182ed8c11a387e05f9fbe540656919cbf83ab8
bruce <bruceoio@hotmail.com>
2024-03-29 19:53:01 +0700

    feat: update

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import configuration from './config/configuration';
 import { ConfigModule } from '@nestjs/config';
-// import { HotelsModule } from './hotels/hotels.module';
+import { HotelsModule } from './hotels/hotels.module';
 @Module({
   imports: [
     AuthModule,
+    HotelsModule,
     ConfigModule.forRoot({
       load: [configuration],
     }),

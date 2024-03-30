1bd5db3fde41eaa162f60fd967f31154564c5974
oOWinOo <peetwin011@hotmail.co.th>
2024-03-30 18:31:07 +0700

    feat(api) : room create update , read.

---
+++ src/room/type.ts
@@ -0,0 +1,16 @@
+export class RoomCreateType {
+    name: string;
+    description?: string;
+    imageURL?: string;
+    price: number;
+    occupancy: number;
+    hotelId: any;
+}
+
+export class RoomUpdateType {
+    name?: string;
+    description?: string;
+    imageURL?: string;
+    price?: number;
+    occupancy?: number;
+}

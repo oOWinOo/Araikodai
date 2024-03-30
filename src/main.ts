1d8ee22c579daf9ca9c8f72209c8fb400e739a2f
bruce <bruceoio@hotmail.com>
2024-03-30 21:06:14 +0700

    add: exception-filter

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './exception-filter/exception.filter';
 
 declare const module: any;
 async function bootstrap() {
@@ -17,6 +18,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(configService.get('PORT'));
 
   if (module.hot) {

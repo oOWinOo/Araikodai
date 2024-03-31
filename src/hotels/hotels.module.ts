56755a5d7b9b05b4277536a55af50f2348b6d5bc
oOWinOo <peetwin011@hotmail.co.th>
2024-03-31 15:19:54 +0700

    fix(module) : add config module to room and hotel module for upload.

--- src/hotels/hotels.module.ts
+++ src/hotels/hotels.module.ts
@@ -3,9 +3,15 @@ import { HotelsController } from './hotels.controller';
 import { HotelsService } from './hotels.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UploadService } from 'src/upload/upload.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [ConfigModule],
   controllers: [HotelsController],
-  providers: [HotelsService, PrismaService, UploadService],
+  providers: [
+    HotelsService, 
+    PrismaService, 
+    UploadService
+  ],
 })
 export class HotelsModule {}
--- src/room/rooms.module.ts
+++ src/room/rooms.module.ts
@@ -3,9 +3,15 @@ import { RoomsController } from './rooms.controller';
 import { RoomsService } from './rooms.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UploadService } from 'src/upload/upload.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [ConfigModule],
   controllers: [RoomsController],
-  providers: [RoomsService, PrismaService , UploadService],
+  providers: [
+    RoomsService,
+    PrismaService,
+    UploadService
+  ],
 })
 export class RoomsModule {}

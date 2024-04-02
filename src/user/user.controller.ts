99f14b62fbf8d64cdae0cdb280241b8e4d78a8bd
oOWinOo <peetwin011@hotmail.co.th>
2024-04-02 14:08:40 +0700

    feat(api) : edit user profile.

---
+++ src/user/user.controller.ts
@@ -0,0 +1,26 @@
+import {
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { Roles } from 'src/roles/roles.decorator';
+import { RolesGuard } from 'src/roles/roles.guard';
+import { UserEditProfile } from './user.dto';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+  constructor(private userServices: UserService) {}
+
+  @Roles(['user'])
+  @UseGuards(AuthGuard, RolesGuard)
+  @HttpCode(HttpStatus.OK)
+  @Patch(':id')
+  async editProfile(@Param('id') userId: number, data: UserEditProfile) {
+    return await this.userServices.updateUser(userId, data);
+  }
+}
